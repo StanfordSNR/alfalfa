dbca0bf999a929a9243ad33b54ff3559c7c36a1a
Francis Y. Yan <fyy@cs.stanford.edu>
2019-11-07 22:27:39 -0800

    .travis-check.sh: fix path to test-suite.log

--- .travis-check.sh
+++ .travis-check.sh
@@ -3,4 +3,4 @@
 cd /home/user/alfalfa
 ./autogen.sh
 ./configure
-make -j3 distcheck V=1 || (cat alfalfa-0.1/_build/src/tests/test-suite.log && exit 1)
+make -j3 distcheck V=1 || (cat alfalfa-0.1/_build/sub/src/tests/test-suite.log && exit 1)
